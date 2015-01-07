5422081948dddc5f6e0ae54ad364e0b47284edcc
leggetter <phil@leggetter.co.uk>
2015-01-07 22:44:13 +0000

    Adding reset function

--- client/src/public/js/BenchmarkRunner.js
+++ client/src/public/js/BenchmarkRunner.js
@@ -3,23 +3,39 @@ function BenchmarkRunner( services, options ) {
 	this._services = services;
 	this._options = options || {};
 
-	this._serviceIndex = 0;
-	this._messageCountTo = 20;
-  this._sendNextUponReceipt = true;
+	this._serviceIndex;
+	this._messageCountTo;
+  this._messageCount;
+  this._sendNextUponReceipt;
 
-  this._testChannelName = 'private-' + ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-	    return v.toString(16);
-		}));
-                
-  this._serviceLatencyResults = {};
-  this._logMessages = [];
-                
+  this._serviceLatencyResults;
+  this._logMessages;
+
+  this._testChannelName;
+
   this._service;
   this._localMsg;
   this._messageCount;
   this._testLatencyResults;
+
+  this.reset();
 }
+
+BenchmarkRunner.prototype.reset = function() {
+  this._serviceIndex = 0;
+  this._messageCountTo = 20;
+  this._sendNextUponReceipt = true;
+
+  this._serviceLatencyResults = {};
+  this._logMessages = [];
+
+  this._testChannelName = 'private-' + ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  }));
+
+  this._testInit();
+};
 
 BenchmarkRunner.prototype._testInit = function() {
   this._tester = null;
@@ -35,7 +51,7 @@ BenchmarkRunner.prototype._testInit = function() {
 // Services listeners
 BenchmarkRunner.prototype.onMessage = function( message ) {
     this.onLog( this._service.name + ': <<< ' + JSON.stringify( message ) );
-    
+
     if( message.timeout != this._localMsg.timeout ) {
         this.onLog( this._service.name + ': <<< received message, but not within 2 seconds' );
     }
@@ -45,14 +61,14 @@ BenchmarkRunner.prototype.onMessage = function( message ) {
         var latency = ( now - sent );
         this._testLatencyResults.push( latency );
         this.onLog( this._service.name + ': <<< latency: ' + latency + 'ms' );
-        
+
         if( this._sendNextUponReceipt ) {
-        
+
             clearTimeout( this._localMsg.timeout );
             this._sendNextMessage();
         }
     }
-};               
+};
 
 BenchmarkRunner.prototype.onReady = function() {
 	this._sendNextMessage();
@@ -65,12 +81,12 @@ BenchmarkRunner.prototype.onLog = function( message ) {
 	}
 };
 ///
-             
+
 BenchmarkRunner.prototype._sendNextMessage = function() {
 	var self = this;
 
   this._messageCount++;
-  
+
   if( this._messageCount < this._messageCountTo ) {
     this._localMsg.time = new Date();
     this._localMsg.timeout = setTimeout( function() {
@@ -78,7 +94,7 @@ BenchmarkRunner.prototype._sendNextMessage = function() {
          		self._sendNextMessage();
          	}, 0 );
         }, 1000);
-    
+
     this.onLog( this._service.name + ': >>> ' + JSON.stringify( this._localMsg ) );
     this._service.send( this._localMsg );
   }
@@ -88,13 +104,13 @@ BenchmarkRunner.prototype._sendNextMessage = function() {
     this.onLog( 'TEST COMPLETE' );
     this.onLog( 'RESULTS: ' + this._testLatencyResults.join( ', ' ) +
                 ' (Average: ' + BenchmarkRunner.getAverage( this._testLatencyResults ) + 'ms)' );
-                 
+
     this._serviceLatencyResults[ this._service.name ] = this._testLatencyResults;
-    
+
     this._nextTest();
   }
 }
-                
+
 BenchmarkRunner.prototype._nextTest = function() {
 	this._service = this._getNextService();
   if( this._service === null ) {
@@ -124,7 +140,7 @@ BenchmarkRunner.prototype._getNextService = function() {
 BenchmarkRunner.prototype.start = function() {
 	this._nextTest();
 };
-                
+
 BenchmarkRunner.getAverage = function( values ) {
 	var total = 0;
   var valueCount = values.length;
